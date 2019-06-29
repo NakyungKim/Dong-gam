589ebf5663b0aef0a431fbe1253841d3739bc4c7
heleneunji <heleneunji@users.noreply.github.com>
2019-06-29 20:18:18 +0900

    log 추가

--- Masterapp/ver.1/app/src/main/java/com/example/otpapplication/Main2Activity.java
+++ Masterapp/ver.1/app/src/main/java/com/example/otpapplication/Main2Activity.java
@@ -3,12 +3,17 @@ package com.example.otpapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class Main2Activity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.e(TAG, "onCreate start in MainActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Log.e(TAG, "onCreate end in MainActivity");
     }
 }
--- Masterapp/ver.1/app/src/main/java/com/example/otpapplication/MainActivity.java
+++ Masterapp/ver.1/app/src/main/java/com/example/otpapplication/MainActivity.java
@@ -6,28 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.e(TAG, "onCreate start in MainActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "onCreate end in MainActivity");
     }
 
-    public void on1(View v) {
+    public void close(View v) {
 
-        Toast.makeText(getApplicationContext(), "닫혔습니다.", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Closed", Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "onCreate close in MainActivity");
 
     }
 
-    public void on2(View v) {
+    public void open(View v) {
 
-        Toast.makeText(getApplicationContext(), "열렸습니다.", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Opended", Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "onCreate open in MainActivity");
     }
 
-    public void on3(View v) {
-
+    public void UserList(View v) {
+        Log.e(TAG, "onCreate userlist in MainActivity");
         Intent intent = new Intent(this, Main2Activity.class);
         startActivity(intent);
     }

a5fbdcb39b79e36629f3356f9dec952e47b8d3a6
heleneunji <heleneunji@users.noreply.github.com>
2019-09-27 22:49:16 +0900

    Update Main2Activity.java

--- Masterapp/ver.1/app/src/main/java/com/example/otpapplication/Main2Activity.java
+++ Masterapp/ver.1/app/src/main/java/com/example/otpapplication/Main2Activity.java
@@ -16,8 +16,4 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         Log.e(TAG, "onCreate end in MainActivity");
     }
-    int temp1 [5] = {id, password, roomnumber, date, OTP}
-    int temp2 [5] = {annung22, gmlcks1234, 201, 2019.09.24, 645396}
-    int temp3 [5] = {heleneunji, dmswl2345, 501, 2019.09.26, 826394}
-    int temp4 [5] = {knk}
 }
